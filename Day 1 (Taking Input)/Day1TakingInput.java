c83a35bf37409af1810c7be6f230b325b9c3c621
Sense <mehershakti1020@gmail.com>
2023-03-02 13:10:51 +0530

    standardized sorting done

--- Day 1 (Taking Input)/Day1TakingInput.java
+++ Day 1 (Taking Input)/Day1TakingInput.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * TakingInput
  */
 
-public class TakingInput {
+public class Day1TakingInput {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
