e0745dcd64dae796f6da793fefab78b9d81efd4e
Sense <mehershakti1020@gmail.com>
2023-03-03 07:19:01 +0530

    Sorted in a standard manner

---
+++ Day 7 Loops/src/NOTE.java
@@ -0,0 +1,30 @@
+/**
+ *      What are iterative statements /Loops?
+ *          - Iterative statements, also known as loops, are programming constructs
+ *              that allow a block of code to be executed repeatedly based on a certain
+ *              condition.
+ *          - They are commonly used when there is a need to perform a repetitive
+ *              task or when the number of iterations is not known in advance.
+ *
+ *          There are several types of loops in most programming languages, including:
+ *
+ *          1. While loop: Executes a block of code repeatedly as long as a specified condition is true.
+ *          2. For loop: Executes a block of code a specific number of times,
+ *              typically based on the size of a collection or an explicit count.
+ *          3. Do-while loop: Executes a block of code at least once, and then
+ *              continues to execute it as long as a specified condition is true.
+ *          3. For-each loop: A special type of loop that is used to iterate over each element in a collection.
+ *          4. Loops are an essential tool for programmers, as they allow for
+ *              efficient and concise code that can be used to process large amounts
+ *              of data or perform repetitive tasks.
+ *
+ */
+
+
+
+
+public class NOTE {
+    public static void main(String[] args) {
+
+    }
+}
